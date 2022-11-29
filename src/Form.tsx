3de8428231f9ea0d11b3011a5789a6f877e30ed1
[jwhong70] <[jwhong70@gmail.com]>
2022-11-29 13:30:17 +0900

    #6 Register - Validation

--- src/Form.tsx
+++ src/Form.tsx
@@ -11,19 +11,56 @@ interface IForm {
 }
 
 function Form() {
-  const { register, handleSubmit } = useForm<IForm>();
+  const { register, handleSubmit } = useForm<IForm>({
+    defaultValues: { email: "@naver.com" },
+  });
   const onValid = (data: IForm) => {};
   return (
     <form
       style={{ display: "flex", flexDirection: "column", width: 300 }}
       onSubmit={handleSubmit(onValid)}
     >
-      <input {...register("email")} placeholder="Email" />
-      <input {...register("firstName")} placeholder="First Name" />
-      <input {...register("lastName")} placeholder="Last Name" />
-      <input {...register("username")} placeholder="Username" />
-      <input {...register("password")} placeholder="Password" />
-      <input {...register("password1")} placeholder="Password1" />
+      <input
+        {...register("email", {
+          required: "Email is required",
+          pattern: {
+            value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+            message: "Only naver.com emails allowed",
+          },
+        })}
+        placeholder="Email"
+      />
+      <input
+        {...register("firstName", {
+          required: "write here",
+          validate: {
+            noNico: (value) =>
+              value.includes("nico") ? "no nicos allowed" : true,
+            noNick: (value) =>
+              value.includes("nick") ? "no nick allowed" : true,
+          },
+        })}
+        placeholder="First Name"
+      />
+      <input
+        {...register("lastName", { required: "write here" })}
+        placeholder="Last Name"
+      />
+      <input
+        {...register("username", { required: "write here", minLength: 10 })}
+        placeholder="Username"
+      />
+      <input
+        {...register("password", { required: "write here", minLength: 5 })}
+        placeholder="Password"
+      />
+      <input
+        {...register("password1", {
+          required: "Password is required",
+          minLength: { value: 5, message: "Your password is too short." },
+        })}
+        placeholder="Password1"
+      />
       <button>Add</button>
     </form>
   );
