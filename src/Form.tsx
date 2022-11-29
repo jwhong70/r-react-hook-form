11bcd1d1768561b8d5b545267371c8371688aa7e
[jwhong70] <[jwhong70@gmail.com]>
2022-11-29 10:51:15 +0900

    #4 Register

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,4 +1,27 @@
+import { useForm } from "react-hook-form";
+
+interface IForm {
+  email: string;
+  firstName: string;
+  lastName: string;
+  username: string;
+  password: string;
+  password1: string;
+  extraError?: string;
+}
+
 function Form() {
-  return null;
+  const { register } = useForm();
+  return (
+    <form>
+      <input {...register("email")} placeholder="Email" />
+      <input {...register("firstName")} placeholder="First Name" />
+      <input {...register("lastName")} placeholder="Last Name" />
+      <input {...register("username")} placeholder="Username" />
+      <input {...register("password")} placeholder="Password" />
+      <input {...register("password1")} placeholder="Password1" />
+      <button>Add</button>
+    </form>
+  );
 }
 export default Form;
